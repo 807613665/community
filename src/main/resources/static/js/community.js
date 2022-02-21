00a377a4da314dd4e8b1ce81a1060d5f12c930fd
807613665 <807613665@qq.com>
2022-02-21 14:15:24 +0800

    未登录跳转登录界面

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -14,7 +14,16 @@ function post(){
             if (response.code==200){
                 $("#comment_textarea").hide();
             }else{
-                alert(response.message);
+                if(response.code==2003){
+                    var isAccepted = confirm(response.message);
+                    console.log(isAccepted);
+                    if(isAccepted){
+                        window.open("https://gitee.com/oauth/authorize?client_id=a8a8ab23769eb09aba2f27efdedbd52946b72ab323dcfda2a7c9118ea0c29f85&redirect_uri=http://localhost:8887/callback/gitee&response_type=code");
+                        window.localStorage.setItem("closabel",true);
+                    }
+                }else {
+                    alert(response.message);
+                }
             }
             console.log(response);
         },
